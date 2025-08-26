3f4e4efbbf44b149354948c53dbe8189095be453
Maria Dana Serban <tigreguapo5@gmail.com>
2025-08-26 22:50:54 +0200

    Mi primera subida

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 80;
+
+app.use(express.static(__dirname));
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+});
